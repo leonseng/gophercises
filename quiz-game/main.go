05d8112b297f89f73c78a1ca2463c40513ba5e36
Leon Seng <w.leon.seng@gmail.com>
2020-08-30 14:11:09 +1000

    Completed quiz part 1

---
+++ quiz-game/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func main() {
+	filename := os.Args[1]
+
+	fmt.Println(filename)
+	csvFile, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("Couldn't open CSV file", err)
+	}
+
+	// parse CSV file
+	r := csv.NewReader(csvFile)
+	var rightCount, wrongCount int
+
+	stdinR := bufio.NewReader(os.Stdin)
+	for {
+		// read question and answer from CSV
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Println(err)
+		}
+
+		q, a := record[0], record[1]
+
+		// present question and expect answer
+		fmt.Printf("Q: %v? ", q)
+
+		userA, _ := stdinR.ReadString('\n')
+		if strings.TrimSpace(userA) == a {
+			rightCount++
+		} else {
+			wrongCount++
+		}
+	}
+
+	fmt.Printf("Score: %v/%v\n", rightCount, rightCount+wrongCount)
+}
