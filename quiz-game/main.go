0f002601650a1216d33d4cd34db14c96270be6a2
Leon Seng <w.leon.seng@gmail.com>
2020-08-30 23:49:28 +1000

    Solved part 2

--- quiz-game/main.go
+++ quiz-game/main.go
@@ -3,47 +3,73 @@ package main
 import (
 	"bufio"
 	"encoding/csv"
+	"flag"
 	"fmt"
-	"io"
 	"os"
 	"strings"
+	"sync"
+	"time"
 )
 
-func main() {
-	filename := os.Args[1]
+var rightCount int
+var aChannel = make(chan string)
+var wg = sync.WaitGroup{}
+var stdinReader = bufio.NewReader(os.Stdin)
 
-	fmt.Println(filename)
-	csvFile, err := os.Open(filename)
+func main() {
+	var gameTime int
+	var fileName string
+
+	flag.IntVar(&gameTime, "t", 30, "Quiz game timer in seconds")
+	flag.StringVar(&fileName, "f", "problems.csv", "CSV file containing problems and answers")
+	flag.Parse()
+
+	problems := loadProblems(fileName)
+
+	fmt.Printf("%v seconds to answer %v questions. Press Enter to start game...", gameTime, len(problems))
+	stdinReader.ReadString('\n')
+
+	wg.Add(1)  // only wait for one of the goroutines to finish
+	go playGame(problems)
+	go gameTimer(gameTime)
+	wg.Wait()
+	
+	fmt.Printf("Score: %v/%v\n", rightCount, len(problems))
+}
+
+func loadProblems(fileName string) [][]string {
+	csvFile, err := os.Open(fileName)
 	if err != nil {
 		fmt.Println("Couldn't open CSV file", err)
 	}
 
 	// parse CSV file
-	r := csv.NewReader(csvFile)
-	var rightCount, wrongCount int
+	lines, err := csv.NewReader(csvFile).ReadAll()
+	if err != nil {
+		panic(err)
+	}
 
-	stdinR := bufio.NewReader(os.Stdin)
-	for {
-		// read question and answer from CSV
-		record, err := r.Read()
-		if err == io.EOF {
-			break
-		} else if err != nil {
-			panic(err)
-		}
+	return lines
+}
 
-		q, a := record[0], record[1]
+func gameTimer(gameTime int) {
+	time.Sleep(time.Duration(gameTime) * time.Second)
+	fmt.Println("\n*** Time's up ***")
+	wg.Done()
+}
+
+func playGame(problems [][]string) {
+	for i, line := range problems {
+		q, a := line[0], line[1]
 
 		// present question and expect answer
-		fmt.Printf("Q: %v? ", q)
+		fmt.Printf("Q%v: %v? ", i+1, q)
+		userA, _ := stdinReader.ReadString('\n')
 
-		userA, _ := stdinR.ReadString('\n')
 		if strings.TrimSpace(userA) == a {
 			rightCount++
-		} else {
-			wrongCount++
 		}
 	}
 
-	fmt.Printf("Score: %v/%v\n", rightCount, rightCount+wrongCount)
+	wg.Done()
 }
